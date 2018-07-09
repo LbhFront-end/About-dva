bbcb5fda7d2531aa46c085c7af62a75fafdbb4aa
Lbh <544289495@qq.com>
2018-07-09 09:51:56 +0800

    延迟删除尝试（effect ）

--- dav-quickstart/src/models/products.js
+++ dav-quickstart/src/models/products.js
@@ -6,4 +6,21 @@ export default {
             return state.filter(item => item.id !== id);
         },
     },
+    effects: {
+        *delete(action, { call, put }) {
+            yield call(function delay(timeout) {
+                return new Promise(resolve => {
+                    setTimeout(resolve, timeout);
+                });
+            }, 2000);
+            yield put({ type: 'delete' });
+        }
+    },
+    methods: {
+        delay(timeout) {
+            return new Promise(resolve => {
+                setTimeout(resolve, timeout);
+            });
+        }
+    }
 };
